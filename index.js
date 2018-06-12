6612d48737319d3f1576f56df94b0d6634ec6cf3
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-12 14:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function scuberGreetingForFeet(feet) {
   let greeting;
   if (feet <= 400) {
-    greeting = "This one is on me!"
-  } 
+    greeting = "This one is on me!";
+  }
+  return greeting;
 }
